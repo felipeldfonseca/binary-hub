f1e202f7ca760e084343ef48dd2d7e3945253b46
Felipe Fonseca <felipeldfonseca@gmail.com>
2025-07-16 22:40:37 -0300

    fix: update ESLint configuration and deployment workflow

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -3,26 +3,36 @@ module.exports = {
   env: {
     es6: true,
     node: true,
+    jest: true
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["tsconfig.json"],
     sourceType: "module",
+    tsconfigRootDir: __dirname
   },
   plugins: [
     "@typescript-eslint"
   ],
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
+    "/lib/**/*",
     ".eslintrc.js",
+    "jest.config.js"
   ],
   rules: {
-    "quotes": ["error", "double"],
+    "quotes": "off",
+    "@typescript-eslint/quotes": ["error", "single"],
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "warn",
-  },
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn", { 
+      "argsIgnorePattern": "^_",
+      "varsIgnorePattern": "^_",
+      "caughtErrorsIgnorePattern": "^_"
+    }]
+  }
 }; 
